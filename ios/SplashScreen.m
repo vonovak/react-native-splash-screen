beaf054cdd3750bdf3d18d5616f636b0b47a3fc4
Vojtech Novak <vonovak@gmail.com>
2018-03-14 14:20:31 +0100

    get rid of the ugly code

--- ios/SplashScreen.m
+++ ios/SplashScreen.m
@@ -10,31 +10,43 @@
 #import "SplashScreen.h"
 #import <React/RCTBridge.h>
 
-static bool waiting = true;
 static bool addedJsLoadErrorObserver = false;
+static UIView* splashViewRef;
+static RCTRootView* rootViewRef;
 
 @implementation SplashScreen
 - (dispatch_queue_t)methodQueue{
     return dispatch_get_main_queue();
 }
+
 RCT_EXPORT_MODULE()
 
-+ (void)show {
++ (void)show:(RCTRootView *)rootview {
     if (!addedJsLoadErrorObserver) {
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(jsLoadError:) name:RCTJavaScriptDidFailToLoadNotification object:nil];
         addedJsLoadErrorObserver = true;
     }
-
-    while (waiting) {
-        NSDate* later = [NSDate dateWithTimeIntervalSinceNow:0.1];
-        [[NSRunLoop mainRunLoop] runUntilDate:later];
-    }
+    
+    UIView *splashView = [[[NSBundle mainBundle] loadNibNamed:@"LaunchScreen" owner:self options:nil] objectAtIndex:0];
+    splashView.frame = rootview.bounds;
+    splashViewRef = splashView;
+    rootViewRef = rootview;
+    [rootview addSubview:splashView];
 }
 
 + (void)hide {
-    dispatch_async(dispatch_get_main_queue(),
-                   ^{
-                       waiting = false;
+    float duration = 0.25;
+    dispatch_time_t delay = 0;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC)),
+                   dispatch_get_main_queue(), ^{
+                       [UIView transitionWithView:rootViewRef
+                                         duration:duration
+                                          options:UIViewAnimationOptionTransitionCrossDissolve
+                                       animations:^{
+                                           splashViewRef.hidden = YES;
+                                       } completion:^(__unused BOOL finished) {
+                                           [splashViewRef removeFromSuperview];
+                                       }];
                    });
 }
 
